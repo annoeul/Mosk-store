4313c55a292d2d02c0265f48a9e7ef1a678a5cee
annoeul <shdmf1137@gmail.com>
2023-11-13 00:17:01 +0900

    commit

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,89 @@
+import {
+  Avatar,
+  Grid,
+  Box,
+  Button,
+  CssBaseline,
+  TextField,
+  Typography,
+  Container,
+} from "@mui/material"
+
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
+import { Link } from "react-router-dom"
+
+export default function SignUpPage() {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="storeName"
+                label="storeName"
+                name="storeName"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link to="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
