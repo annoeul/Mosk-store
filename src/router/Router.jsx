de491c72af5525799a3cd531a09e718ed88a7e72
annoeul <shdmf1137@gmail.com>
2023-11-13 09:45:51 +0900

    feat : router추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import LoginPage from "../pages/LoginPage"
 import SignupPage from "../pages/SignupPage"
+import Home from "../pages/Home"
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signUp" element={<SignupPage />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   )
 }
