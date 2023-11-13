aebc1e8559220c3e3f6909efa00dc720659b0201
annoeul <shdmf1137@gmail.com>
2023-11-13 14:41:40 +0900

    feat: product 관련 컴포넌트 생성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react"
 import Header from "../components/Header"
+import { Grid } from "@mui/material"
+
+import ProductListPage from "./ProductListPage"
+import CategoryListPage from "./CategoryListPage"
 
 function Home() {
   return (
     <div>
       <Header />
+      <Grid container>
+        <Grid
+          item
+          xs={2}
+          sx={{
+            borderRight: "1px solid black",
+
+            textAlign: "center",
+            height: "100vh",
+          }}
+        >
+          <CategoryListPage />
+        </Grid>
+        <Grid item xs={10}>
+          <ProductListPage />
+        </Grid>
+      </Grid>
     </div>
   )
 }
