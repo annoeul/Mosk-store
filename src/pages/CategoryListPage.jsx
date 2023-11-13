ebf15faa726698ceebe1cfbcf0f276211db607cd
annoeul <shdmf1137@gmail.com>
2023-11-13 22:36:27 +0900

    feat : category 기능 구현 중

--- src/pages/CategoryListPage.jsx
+++ src/pages/CategoryListPage.jsx
@@ -1,12 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import Btn from "../components/Btn"
-import { Container } from "@mui/material"
+import { Box, ButtonGroup, Container, Grid, TextField } from "@mui/material"
+import Category from "../components/Category"
 
 function CategoryListPage() {
+  const [isCheck, setIsCheck] = useState(false)
+
+  const onClick = () => {
+    setIsCheck(!isCheck)
+  }
   return (
-    <Container>
-      <Btn variant="outlined" text="카테고리 생성" size="large" sx={{ m: 2 }} />
-    </Container>
+    <>
+      <Btn
+        variant="outlined"
+        text="카테고리 생성"
+        size="large"
+        sx={{ my: 2 }}
+        onClick={onClick}
+      />
+      {isCheck ? (
+        <Box>
+          <TextField
+            id="standard-basic"
+            label="카테고리 이름"
+            variant="standard"
+            sx={{ m: 2 }}
+          />
+          <ButtonGroup sx={{ m: 2 }}>
+            <Btn variant="outlined" text="취소" />
+            <Btn variant="contained" text="생성" />
+          </ButtonGroup>
+          <Category />
+        </Box>
+      ) : (
+        <Category />
+      )}
+    </>
   )
 }
 
