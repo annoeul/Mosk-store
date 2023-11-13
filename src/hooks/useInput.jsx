e3112f0ad145ab2f0e9cac1170f237d79f1bd8cb
annoeul <shdmf1137@gmail.com>
2023-11-13 10:12:48 +0900

    feat : hooks생성

---
+++ src/hooks/useInput.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+
+function useInput(initialValue) {
+  const [userInput, setUserInput] = useState(initialValue)
+  const onChange = (e) => {
+    const { name, value } = e.target
+    setUserInput({ ...userInput, [name]: value })
+    console.log(userInput)
+  }
+  return { userInput, onChange }
+}
+
+export default useInput
