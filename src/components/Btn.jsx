0fd3407a12f4eae1590369fc2e7d99d0195ceeb5
annoeul <shdmf1137@gmail.com>
2023-11-13 22:36:52 +0900

    design : 레이아웃 수정 중

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { Button } from "@mui/material"
 
-function Btn({ size, text, variant, sx }) {
+function Btn({ size, text, variant, sx, onClick }) {
   return (
-    <Button variant={variant} sx={sx} size={size}>
+    <Button variant={variant} sx={sx} size={size} onClick={onClick}>
       {text}
     </Button>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,21 +21,21 @@ function Header() {
     }
   }
 
-  const logout = async () => {
-    try {
-      const response = await api.delete("/auth")
-      const getToken = localStorage.getItem("accessToken")
-      const checkLogout = window.confirm("로그아웃 하시겠습니까?")
-      if (checkLogout) {
-        localStorage.removeItem("accessToken")
-        navigate("/")
-      } else {
-        return
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  // const logout = async () => {
+  //   try {
+  //     const response = await api.delete("/auth")
+  //     const getToken = localStorage.getItem("accessToken")
+  //     const checkLogout = window.confirm("로그아웃 하시겠습니까?")
+  //     if (checkLogout) {
+  //       localStorage.removeItem("accessToken")
+  //       navigate("/")
+  //     } else {
+  //       return
+  //     }
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
   useEffect(() => {
     checkLogin()
@@ -43,21 +43,19 @@ function Header() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: "#dfdbdb" }}>
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
-            color="primary"
+            // size="large"
+            // edge="start"
+            // color="black"
             arai-label="menu"
             sx={{ mr: 2 }}
           ></IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             StoreName
           </Typography>
-          <Button color="inherit" onClick={logout}>
-            {isLogin ? "Logout" : ""}
-          </Button>
+          <Button color="inherit">{isLogin ? "Logout" : ""}</Button>
         </Toolbar>
       </AppBar>
     </Box>
